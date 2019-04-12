cc9171c63435b3eb63f5181aa56216e15d8696e8
Matt Corsaro <matthew_corsaro@brown.edu>
2019-04-11 20:54:25 -0400

    Source each camera's calibration file so calibration values are added to environment variables.

--- kuka_cam.sh
+++ kuka_cam.sh
@@ -3,3 +3,9 @@ export CAM_2_SERIAL=829212072292
 export CAM_3_SERIAL=829212071322
 export CAM_4_SERIAL=829212071682
 export CAM_5_SERIAL=829212072309
+
+source $KUKA_WS_PATH/src/kuka_brown/kuka_cam/calib/cam_1_calib.sh
+source $KUKA_WS_PATH/src/kuka_brown/kuka_cam/calib/cam_2_calib.sh
+source $KUKA_WS_PATH/src/kuka_brown/kuka_cam/calib/cam_3_calib.sh
+source $KUKA_WS_PATH/src/kuka_brown/kuka_cam/calib/cam_4_calib.sh
+source $KUKA_WS_PATH/src/kuka_brown/kuka_cam/calib/cam_5_calib.sh
