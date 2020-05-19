f4afad1e036916f4363d9c94cee2969a43970435
Ben Abbatematteo <abba@brown.edu>
2020-05-19 11:29:51 -0400

    pushing back pointcloud and timestamp

--- src/cloud_processor.cpp
+++ src/cloud_processor.cpp
@@ -17,12 +17,23 @@ namespace cloud_processor
     std::string camera_name = msg->header.frame_id;
     double timestamp = msg->header.stamp.toSec();
 
-    // TODO: fix warning "failed to field matching field rgb."
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = boost::shared_ptr<pcl::PointCloud<pcl::PointXYZ> >(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::fromROSMsg(*msg, *cloud);
 
+    std::pair<pcl::PointCloud<pcl::PointXYZ>::Ptr, double> my_pair;
+    my_pair.first = cloud;
+    my_pair.second = timestamp;
+    cloud_map_[camera_name].push_back(my_pair);
+
+    if (cloud_map_[camera_name].size() > max_deque_size_)
+    {
+      cloud_map_[camera_name].pop_front();
+    }
+
+    ROS_INFO_STREAM("----------");
     ROS_INFO_STREAM(std::setprecision(20) << timestamp << std::endl);
     ROS_INFO_STREAM(camera_name << std::endl);
+    ROS_INFO_STREAM(cloud_map_[camera_name].size());
 
   }
   void CloudProcessor::combineClouds(){
